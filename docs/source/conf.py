c23c2f30cd5845235b3f62a49926be7bc4c49090
AnjaConev <anjaconev@gmail.com>
2023-05-25 21:04:17 -0500

    Docs logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,12 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "engens_logo.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
